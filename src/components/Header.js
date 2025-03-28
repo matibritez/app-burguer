98ba735d71860332b1a51ed114ef2ad5b5d7ae38
unknown <matiasbritez36@gmail.com>
2025-03-27 21:22:35 -0300

    header

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Header(){
+    return(
+        <header>
+            <h1>Hamburguesas 90s</h1>
+            <nav>
+                <a href='#menu'>Menu</a>
+                <a href='#cart'>Carrito</a>
+            </nav>
+        </header>
+    )
+}
